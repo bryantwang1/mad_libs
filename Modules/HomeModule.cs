4ac30a413a1bf640dbc1cc0676431b8151d4b03d
Bryant Wang and Levi Bibo <bryant+levi@>
2016-11-14 11:37:44 -0800

    Complete the lesson.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -16,8 +16,11 @@ namespace MadLibs
             myStoryVariables.SetPerson2(Request.Query["person2"]);
             myStoryVariables.SetAnimal(Request.Query["animal"]);
             myStoryVariables.SetExclamation(Request.Query["exclamation"]);
-            myStoryVariables.SetVerb(Request.Query["verb"]);
-            myStoryVariables.SetNoun(Request.Query["noun"]);
+            myStoryVariables.SetVerb1(Request.Query["verb1"]);
+            myStoryVariables.SetVerb2(Request.Query["verb2"]);
+            myStoryVariables.SetVerb3(Request.Query["verb3"]);
+            myStoryVariables.SetNoun1(Request.Query["noun1"]);
+            myStoryVariables.SetNoun2(Request.Query["noun2"]);
             return View["story.cshtml", myStoryVariables];
             };
         }
